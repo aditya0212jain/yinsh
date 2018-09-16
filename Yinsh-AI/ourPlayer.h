41791849514a6678bc326a25926da64773ae8bf8
aditya0212jain <aditya0212jain@gmail.com>
2018-09-16 16:46:24 -0700

    debug starting

--- Yinsh-AI/ourPlayer.h
+++ Yinsh-AI/ourPlayer.h
@@ -70,7 +70,7 @@ public:
   unordered_map <string,transpositionNode> htMap;
   void play();
   string minimax_decision(ourGame game);
-  struct transitionMove minimax(int depth,bool isMax,long long int alpha,long long int beta);
+  struct transitionMove minimax(int depth,bool isMax,long long int alpha,long long int beta,int max_depth);
   struct transitionMove idMinimax(int max_depth,double maxTime);
   vector<string> sortChildren(vector<string> moves,bool forMax);
 
